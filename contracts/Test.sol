34ece3fceaf67ba9546a302de6e570c7ac490bcc
yukimura <yuki.imamura.all@gmail.com>
2023-03-08 22:46:49 +0900

    Update sample code

---
+++ contracts/Test.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.15;
+
+contract Sample {
+    function HelloWorld() public pure returns (string memory) {
+        return "Hello World";
+    }
+}
